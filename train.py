7a21d3db8fcb096ab1f5c0435f443da664ebb309
Alexander Kalinovsky (bigcat) <gakarak@gmail.com>
2019-07-17 14:18:53 +0300

    code refactoring

--- train.py
+++ train.py
@@ -15,146 +15,175 @@ from data_utils import TrainDatasetFromFolder, ValDatasetFromFolder, display_tra
 from loss import GeneratorLoss
 from model import Generator, Discriminator
 
-parser = argparse.ArgumentParser(description='Train Super Resolution Models')
-parser.add_argument('--crop_size', default=88, type=int, help='training images crop size')
-parser.add_argument('--upscale_factor', default=4, type=int, choices=[2, 4, 8],
-                    help='super resolution upscale factor')
-parser.add_argument('--num_epochs', default=100, type=int, help='train epoch number')
 
-opt = parser.parse_args()
+def train_step():
 
-CROP_SIZE = opt.crop_size
-UPSCALE_FACTOR = opt.upscale_factor
-NUM_EPOCHS = opt.num_epochs
 
-train_set = TrainDatasetFromFolder('data/VOC2012/train', crop_size=CROP_SIZE, upscale_factor=UPSCALE_FACTOR)
-val_set = ValDatasetFromFolder('data/VOC2012/val', upscale_factor=UPSCALE_FACTOR)
-train_loader = DataLoader(dataset=train_set, num_workers=4, batch_size=64, shuffle=True)
-val_loader = DataLoader(dataset=val_set, num_workers=4, batch_size=1, shuffle=False)
+    print('-')
 
-netG = Generator(UPSCALE_FACTOR)
-print('# generator parameters:', sum(param.numel() for param in netG.parameters()))
-netD = Discriminator()
-print('# discriminator parameters:', sum(param.numel() for param in netD.parameters()))
 
-generator_criterion = GeneratorLoss()
 
-if torch.cuda.is_available():
-    netG.cuda()
-    netD.cuda()
-    generator_criterion.cuda()
 
-optimizerG = optim.Adam(netG.parameters())
-optimizerD = optim.Adam(netD.parameters())
+def main_train(path_trn: str, path_val: str,
+               crop_size: int, upscale_factor: int, num_epochs: int, num_workers: int):
 
-results = {'d_loss': [], 'g_loss': [], 'd_score': [], 'g_score': [], 'psnr': [], 'ssim': []}
+    train_set = TrainDatasetFromFolder('data/VOC2012/train', crop_size=crop_size, upscale_factor=upscale_factor)
+    val_set = ValDatasetFromFolder('data/VOC2012/val', upscale_factor=upscale_factor)
 
-for epoch in range(1, NUM_EPOCHS + 1):
-    train_bar = tqdm(train_loader)
-    running_results = {'batch_sizes': 0, 'd_loss': 0, 'g_loss': 0, 'd_score': 0, 'g_score': 0}
 
-    netG.train()
-    netD.train()
-    for data, target in train_bar:
-        g_update_first = True
-        batch_size = data.size(0)
-        running_results['batch_sizes'] += batch_size
+    #
+    train_loader = DataLoader(dataset=train_set, num_workers=4, batch_size=64, shuffle=True)
+    val_loader = DataLoader(dataset=val_set, num_workers=4, batch_size=1, shuffle=False)
 
-        ############################
-        # (1) Update D network: maximize D(x)-1-D(G(z))
-        ###########################
-        real_img = Variable(target)
-        if torch.cuda.is_available():
-            real_img = real_img.cuda()
-        z = Variable(data)
-        if torch.cuda.is_available():
-            z = z.cuda()
-        fake_img = netG(z)
+    netG = Generator(upscale_factor)
+    print('# generator parameters:', sum(param.numel() for param in netG.parameters()))
+    netD = Discriminator()
+    print('# discriminator parameters:', sum(param.numel() for param in netD.parameters()))
 
-        netD.zero_grad()
-        real_out = netD(real_img).mean()
-        fake_out = netD(fake_img).mean()
-        d_loss = 1 - real_out + fake_out
-        d_loss.backward(retain_graph=True)
-        optimizerD.step()
+    generator_criterion = GeneratorLoss()
 
-        ############################
-        # (2) Update G network: minimize 1-D(G(z)) + Perception Loss + Image Loss + TV Loss
-        ###########################
-        netG.zero_grad()
-        g_loss = generator_criterion(fake_out, fake_img, real_img)
-        g_loss.backward()
-        optimizerG.step()
-        fake_img = netG(z)
-        fake_out = netD(fake_img).mean()
+    if torch.cuda.is_available():
+        netG.cuda()
+        netD.cuda()
+        generator_criterion.cuda()
 
-        g_loss = generator_criterion(fake_out, fake_img, real_img)
-        running_results['g_loss'] += g_loss.data[0] * batch_size
-        d_loss = 1 - real_out + fake_out
-        running_results['d_loss'] += d_loss.data[0] * batch_size
-        running_results['d_score'] += real_out.data[0] * batch_size
-        running_results['g_score'] += fake_out.data[0] * batch_size
+    optimizerG = optim.Adam(netG.parameters())
+    optimizerD = optim.Adam(netD.parameters())
 
-        train_bar.set_description(desc='[%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f' % (
-            epoch, NUM_EPOCHS, running_results['d_loss'] / running_results['batch_sizes'],
-            running_results['g_loss'] / running_results['batch_sizes'],
-            running_results['d_score'] / running_results['batch_sizes'],
-            running_results['g_score'] / running_results['batch_sizes']))
+    results = {'d_loss': [], 'g_loss': [], 'd_score': [], 'g_score': [], 'psnr': [], 'ssim': []}
 
-    netG.eval()
-    out_path = 'training_results/SRF_' + str(UPSCALE_FACTOR) + '/'
-    if not os.path.exists(out_path):
-        os.makedirs(out_path)
-    val_bar = tqdm(val_loader)
-    valing_results = {'mse': 0, 'ssims': 0, 'psnr': 0, 'ssim': 0, 'batch_sizes': 0}
-    val_images = []
-    for val_lr, val_hr_restore, val_hr in val_bar:
-        batch_size = val_lr.size(0)
-        valing_results['batch_sizes'] += batch_size
-        lr = Variable(val_lr, volatile=True)
-        hr = Variable(val_hr, volatile=True)
-        if torch.cuda.is_available():
-            lr = lr.cuda()
-            hr = hr.cuda()
-        sr = netG(lr)
+    for epoch in range(1, num_epochs + 1):
+        train_bar = tqdm(train_loader)
+        running_results = {'batch_sizes': 0, 'd_loss': 0, 'g_loss': 0, 'd_score': 0, 'g_score': 0}
 
-        batch_mse = ((sr - hr) ** 2).data.mean()
-        valing_results['mse'] += batch_mse * batch_size
-        batch_ssim = pytorch_ssim.ssim(sr, hr).data[0]
-        valing_results['ssims'] += batch_ssim * batch_size
-        valing_results['psnr'] = 10 * log10(1 / (valing_results['mse'] / valing_results['batch_sizes']))
-        valing_results['ssim'] = valing_results['ssims'] / valing_results['batch_sizes']
-        val_bar.set_description(
-            desc='[converting LR images to SR images] PSNR: %.4f dB SSIM: %.4f' % (
-                valing_results['psnr'], valing_results['ssim']))
+        netG.train()
+        netD.train()
+        for data, target in train_bar:
+            g_update_first = True
+            batch_size = data.size(0)
+            running_results['batch_sizes'] += batch_size
 
-        val_images.extend(
-            [display_transform()(val_hr_restore.squeeze(0)), display_transform()(hr.data.cpu().squeeze(0)),
-             display_transform()(sr.data.cpu().squeeze(0))])
-    val_images = torch.stack(val_images)
-    val_images = torch.chunk(val_images, val_images.size(0) // 15)
-    val_save_bar = tqdm(val_images, desc='[saving training results]')
-    index = 1
-    for image in val_save_bar:
-        image = utils.make_grid(image, nrow=3, padding=5)
-        utils.save_image(image, out_path + 'epoch_%d_index_%d.png' % (epoch, index), padding=5)
-        index += 1
+            ############################
+            # (1) Update D network: maximize D(x)-1-D(G(z))
+            ###########################
+            real_img = Variable(target)
+            if torch.cuda.is_available():
+                real_img = real_img.cuda()
+            z = Variable(data)
+            if torch.cuda.is_available():
+                z = z.cuda()
+            fake_img = netG(z)
 
-    # save model parameters
-    torch.save(netG.state_dict(), 'epochs/netG_epoch_%d_%d.pth' % (UPSCALE_FACTOR, epoch))
-    torch.save(netD.state_dict(), 'epochs/netD_epoch_%d_%d.pth' % (UPSCALE_FACTOR, epoch))
-    # save loss\scores\psnr\ssim
-    results['d_loss'].append(running_results['d_loss'] / running_results['batch_sizes'])
-    results['g_loss'].append(running_results['g_loss'] / running_results['batch_sizes'])
-    results['d_score'].append(running_results['d_score'] / running_results['batch_sizes'])
-    results['g_score'].append(running_results['g_score'] / running_results['batch_sizes'])
-    results['psnr'].append(valing_results['psnr'])
-    results['ssim'].append(valing_results['ssim'])
+            netD.zero_grad()
+            real_out = netD(real_img).mean()
+            fake_out = netD(fake_img).mean()
+            d_loss = 1 - real_out + fake_out
+            d_loss.backward(retain_graph=True)
+            optimizerD.step()
 
-    if epoch % 10 == 0 and epoch != 0:
-        out_path = 'statistics/'
-        data_frame = pd.DataFrame(
-            data={'Loss_D': results['d_loss'], 'Loss_G': results['g_loss'], 'Score_D': results['d_score'],
-                  'Score_G': results['g_score'], 'PSNR': results['psnr'], 'SSIM': results['ssim']},
-            index=range(1, epoch + 1))
-        data_frame.to_csv(out_path + 'srf_' + str(UPSCALE_FACTOR) + '_train_results.csv', index_label='Epoch')
+            ############################
+            # (2) Update G network: minimize 1-D(G(z)) + Perception Loss + Image Loss + TV Loss
+            ###########################
+            netG.zero_grad()
+            g_loss = generator_criterion(fake_out, fake_img, real_img)
+            g_loss.backward()
+            optimizerG.step()
+            fake_img = netG(z)
+            fake_out = netD(fake_img).mean()
+
+            g_loss = generator_criterion(fake_out, fake_img, real_img)
+            running_results['g_loss'] += g_loss.data[0] * batch_size
+            d_loss = 1 - real_out + fake_out
+            running_results['d_loss'] += d_loss.data[0] * batch_size
+            running_results['d_score'] += real_out.data[0] * batch_size
+            running_results['g_score'] += fake_out.data[0] * batch_size
+
+            train_bar.set_description(desc='[%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f' % (
+                epoch, num_epochs, running_results['d_loss'] / running_results['batch_sizes'],
+                running_results['g_loss'] / running_results['batch_sizes'],
+                running_results['d_score'] / running_results['batch_sizes'],
+                running_results['g_score'] / running_results['batch_sizes']))
+
+        netG.eval()
+        out_path = 'training_results/SRF_' + str(upscale_factor) + '/'
+        if not os.path.exists(out_path):
+            os.makedirs(out_path)
+        val_bar = tqdm(val_loader)
+        valing_results = {'mse': 0, 'ssims': 0, 'psnr': 0, 'ssim': 0, 'batch_sizes': 0}
+        val_images = []
+        for val_lr, val_hr_restore, val_hr in val_bar:
+            batch_size = val_lr.size(0)
+            valing_results['batch_sizes'] += batch_size
+            lr = Variable(val_lr, volatile=True)
+            hr = Variable(val_hr, volatile=True)
+            if torch.cuda.is_available():
+                lr = lr.cuda()
+                hr = hr.cuda()
+            sr = netG(lr)
+
+            batch_mse = ((sr - hr) ** 2).data.mean()
+            valing_results['mse'] += batch_mse * batch_size
+            batch_ssim = pytorch_ssim.ssim(sr, hr).data[0]
+            valing_results['ssims'] += batch_ssim * batch_size
+            valing_results['psnr'] = 10 * log10(1 / (valing_results['mse'] / valing_results['batch_sizes']))
+            valing_results['ssim'] = valing_results['ssims'] / valing_results['batch_sizes']
+            val_bar.set_description(
+                desc='[converting LR images to SR images] PSNR: %.4f dB SSIM: %.4f' % (
+                    valing_results['psnr'], valing_results['ssim']))
+
+            val_images.extend(
+                [display_transform()(val_hr_restore.squeeze(0)), display_transform()(hr.data.cpu().squeeze(0)),
+                 display_transform()(sr.data.cpu().squeeze(0))])
+        val_images = torch.stack(val_images)
+        val_images = torch.chunk(val_images, val_images.size(0) // 15)
+        val_save_bar = tqdm(val_images, desc='[saving training results]')
+        index = 1
+        for image in val_save_bar:
+            image = utils.make_grid(image, nrow=3, padding=5)
+            utils.save_image(image, out_path + 'epoch_%d_index_%d.png' % (epoch, index), padding=5)
+            index += 1
+
+        # save model parameters
+        torch.save(netG.state_dict(), 'epochs/netG_epoch_%d_%d.pth' % (upscale_factor, epoch))
+        torch.save(netD.state_dict(), 'epochs/netD_epoch_%d_%d.pth' % (upscale_factor, epoch))
+        # save loss\scores\psnr\ssim
+        results['d_loss'].append(running_results['d_loss'] / running_results['batch_sizes'])
+        results['g_loss'].append(running_results['g_loss'] / running_results['batch_sizes'])
+        results['d_score'].append(running_results['d_score'] / running_results['batch_sizes'])
+        results['g_score'].append(running_results['g_score'] / running_results['batch_sizes'])
+        results['psnr'].append(valing_results['psnr'])
+        results['ssim'].append(valing_results['ssim'])
+
+        if epoch % 10 == 0 and epoch != 0:
+            out_path = 'statistics/'
+            data_frame = pd.DataFrame(
+                data={'Loss_D': results['d_loss'], 'Loss_G': results['g_loss'], 'Score_D': results['d_score'],
+                      'Score_G': results['g_score'], 'PSNR': results['psnr'], 'SSIM': results['ssim']},
+                index=range(1, epoch + 1))
+            data_frame.to_csv(out_path + 'srf_' + str(upscale_factor) + '_train_results.csv', index_label='Epoch')
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Train Super Resolution Models')
+    parser.add_argument('--trn', default=None, type=str, required=True, help='path to train dataset')
+    parser.add_argument('--val', default=None, type=str, required=True, help='path to validation dataset')
+    parser.add_argument('--crop_size', default=88, type=int, help='training images crop size')
+    parser.add_argument('--upscale_factor', default=4, type=int, choices=[2, 4, 8],
+                        help='super resolution upscale factor')
+    parser.add_argument('--num_epochs', default=100, type=int, help='train epoch number')
+    parser.add_argument('--threads', default=1, type=int, help='#workers for parallel processing')
+    args = parser.parse_args()
+    print('args:\n\t{}'.format(args))
+    #
+    main_train(
+        path_trn=args.trn,
+        path_val=args.val,
+        crop_size=args.crop_size,
+        upscale_factor=args.upscale_factor,
+        num_epochs=args.num_epochs,
+        num_workers=args.threads
+    )
+
+
+
